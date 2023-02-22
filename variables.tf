f5f693b8f5225f4f6505c6343f8794b87bd9dede
schan90 <qnas90@gmail.com>
2023-02-22 12:22:31 +0900

    re-albc

--- variables.tf
+++ variables.tf
@@ -1,17 +1,12 @@
 ############################# main : var #######################################
 
 variable "env" { type = string }
-# variable "eks_tfstat_path" { type = string }
-# variable "aws_profile" { type = string }
 variable "aws_region" { type = string }
 variable "project" { type = string }
 variable "default_tags" { type = map(string) }
 
 ############################# bknd & vpc & network about : var #######################################
-
-# variable "backend_s3_bucket_name" { default = "" }
-# variable "infra_network_s3_key" { default = "" }
-# variable "eks_s3_key" { default = "" }
+variable "vpc_id" { default = "" }
 variable "tfc_org" { default = "" }
 variable "tfc_wk" { default = "" }
 ############################# eks-cluster : var #######################################
@@ -30,7 +25,7 @@ variable "helm_app_version" { default = "" }
 variable "helm_repository_url" { default = "" }
 variable "create_namespace" { default = false }
 variable "namespace" { default = "kube-system" }
-variable "vpc_id" { default = "" }
+
 variable "replica_count" { default = 1 }
 variable "resources" { default = "" }
 variable "affinity" { default = "" }
